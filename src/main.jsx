e25a68f65a4eeac72e1a10bf9bafca4c5a5f9839
Ruchir Chincholkar <ruchirc@64-squares.com>
2026-02-24 23:42:19 +0530

    Finalize website configuration for www.orivenza.com deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/orivenza-site/" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
