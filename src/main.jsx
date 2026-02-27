308afdfbaf1f6d2bb0c292806d1d396ff44d1874
Ruchir Chincholkar <ruchirc@64-squares.com>
2026-02-27 15:54:02 +0530

    Remove CSS request chain by inlining app styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-const loadAppStyles = () => import('./styles.css')
-if (typeof window !== 'undefined') {
-  window.requestAnimationFrame(() => {
-    setTimeout(loadAppStyles, 0)
-  })
-}
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
