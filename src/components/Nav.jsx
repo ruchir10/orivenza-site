23916f4fc44157dbd189f606756730c96eccc515
Ruchir Chincholkar <ruchirc@64-squares.com>
2026-02-27 16:03:52 +0530

    Restore original Orivenza logo usage

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logoMark from '../assets/orivenza-mark.svg'
 
 export default function Nav() {
   return (
     <header className="nav">
       <div className="nav-inner">
         <Link to="/" className="brand">
-          <img src={logoMark} alt="Orivenza logo" className="logo-mark" width="40" height="40" />
+          <img src="/Orivenza.png" alt="Orivenza logo" className="logo-mark" width="40" height="40" />
           <span className="brand-name">Orivenza</span>
         </Link>
         <nav className="links">
